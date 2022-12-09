c3e0871bb17a70ee541717e683505d2897ba23b9
Phạm Quốc Cường <dok2308@gmail.com>
2022-12-09 22:08:54 +0700

    Fix nhập điểm

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -95,7 +95,7 @@ async function NhapDiem(req, res) {
         return res.json({
             statusCode: 200,
             message: 'Nhập điểm thành công',
-            result: result.result.recordsets[0]
+            
         });
     } else {
         return res.json({
@@ -108,4 +108,94 @@ async function NhapDiem(req, res) {
 
 exports.DanhSachHocSinhTrongLop = DanhSachHocSinhTrongLop;
 exports.NhapDiem = NhapDiem;
+
+async function DanhSachDiem (req, res) {
+    let DanhSachNamHoc;
+    req.MaLop = req.params.MaLop;
+
+    if(!req.body.NamHoc) {
+        DanhSachNamHoc = await usersModel.DanhSachNamHoc();
+        req.body.NamHoc = DanhSachNamHoc.result.recordsets[0][0].NamHoc;
+        return res.json({
+            statusCode: 200,
+            message: 'Lấy danh sách năm học thành công',
+            result: DanhSachNamHoc.result.recordsets[0]
+        });
+    }
+
+    let MaMH = 'TOAN10'
+    let MaLop = req.params.MaLop;
+    let result = await usersModel.DanhSachDiem(MaMH, MaLop, req.body.HocKy, req.body.NamHoc);
+    console.log(result.result)
+    console.log(result.result.length)
+
+    // let data = [{
+    //     MaHS: '20521361',
+    //     HoTen: 'Đỗ Hữu Khánh Hưng',
+    //     MaLHKT: 'DM',
+    //     Diem: 10,
+    //     MaLop: 'TN101'
+    //   },
+    //   {
+    //     MaHS: '20521361',
+    //     HoTen: 'Đỗ Hữu Khánh Hưng',
+    //     MaLHKT: 'KT15P',
+    //     Diem: 10,
+    //     MaLop: 'TN101'
+    //   }];
+
+    let kq = [];
+    let index = 0;
+    for (let i = 0, j = 0; i < result.result.length; i++) {
+        console.log(result.result[i].MaHS)
+        
+        // if( || kq[index].MaHS != result.result[i].MaHS)
+        try {
+            if(kq[index-1].MaHS != result.result[i].MaHS) 
+                kq[index++] = {
+                    MaHS: result.result[i].MaHS,
+                    HoTen: result.result[i].HoTen,
+                    DM: '',
+                    KT15P: '',
+                    KT1T: '',
+                    KTGK: '',
+                    KTCK: '',
+                }
+        } catch (error) {
+            kq[index++] = {
+                MaHS: result.result[i].MaHS,
+                HoTen: result.result[i].HoTen,
+                DM: '',
+                KT15P: '',
+                KT1T: '',
+                KTGK: '',
+                KTCK: '',
+            }
+        }
+        
+        for (j = i; j < result.result.length; j++) {
+            if (result.result[i].MaHS === result.result[j].MaHS && result.result[j].MaLHKT != null)
+                kq[index - 1][`${result.result[j].MaLHKT}`] = result.result[j].Diem;
+            else 
+                break;
+        }
+        // i = j;
+    }
+    
+    // if(result.statusCode === 200) {
+    //     return res.json({
+    //         statusCode: 200,
+    //         message: 'Lấy danh sách học sinh trong lớp và điểm thành công',
+
+    //     });
+    // console.log(results)
+    return res.json({
+        statusCode: 200,
+        message: 'Lấy danh sách học sinh trong lớp và điểm thành công',
+        result: kq
+    });
+}
+
+exports.DanhSachDiem = DanhSachDiem;
+
 
